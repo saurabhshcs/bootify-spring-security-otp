258502cc6d3bf8d51853c8c0ef2523bbc28c1bac
Saurabh Sharma <saurabhshcs@gmail.com>
2020-11-24 23:13:09 +0000

    Initial Second Commit-Project structure.

---
+++ src/main/java/com/techsharezone/bootifyspringsecurityotp/BootifySpringSecurityOtpApplication.java
@@ -0,0 +1,13 @@
+package com.techsharezone.bootifyspringsecurityotp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifySpringSecurityOtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootifySpringSecurityOtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techsharezone/bootifyspringsecurityotp/BootifySpringSecurityOtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsharezone.bootifyspringsecurityotp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootifySpringSecurityOtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
